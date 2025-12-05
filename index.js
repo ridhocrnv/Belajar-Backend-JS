7bcf10cfa53d693133e56561184d1ebf884a3269
ridhocrnv <ridhoahmadirawan281@gmail.com>
2025-12-06 01:15:08 +0800

    Add database query to fetch mahasiswa records

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const koneksi = require('./connection');
+
+koneksi.query('SELECT * FROM mahasiswa', (err, results) => {
+    if (err) {
+        console.error('Error executing query:', err.stack);
+        return;
+    }
+
+    console.log('Query results:', results);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
